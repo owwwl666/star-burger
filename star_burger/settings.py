23ee6c191f681a23f7c36b157bd5bf22042bad85
owwwl666 <zaharsalam228@gmail.com>
2023-12-03 12:39:57 +1000

    added api key for Yandex geocoder.

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -11,6 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SECRET_KEY = env('SECRET_KEY')
+YANDEX_APIKEY = env('YANDEX_APIKEY')
 DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
