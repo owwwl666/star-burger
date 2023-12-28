34f8d0df7fdf146b347430df726a4baaa569165b
root <root@2317164-sy02094.twc1.net>
2023-12-28 14:01:32 +0300

    Revert "installed gunicorn"
    This reverts commit 733b19760c7d7bdbf91a576522171d21f8728ec4.

--- foodcartapp/urls.py
+++ foodcartapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import product_list_api, banners_list_api, register_order,get_my_ip
+from .views import product_list_api, banners_list_api, register_order
 
 
 app_name = "foodcartapp"
@@ -9,5 +9,4 @@ urlpatterns = [
     path("products/", product_list_api),
     path("banners/", banners_list_api),
     path("order/", register_order),
-    path("ip/", get_my_ip),
 ]
